f23de991c3e75b5e39881b4d66abeedf21e0568c
MaximMaximov <maxim.maximov@emergn.com>
2019-03-28 10:59:06 +0300

    Create backend v1

---
+++ index.js
@@ -0,0 +1,72 @@
+var express = require('express');
+var bodyParser = require("body-parser");
+
+var app = express();
+
+var state = require('./state').state;
+
+//Here we are configuring express to use body-parser as middle-ware.
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+app.get('/api/configurations', (req, res) => {
+    return res.status(200).send(
+        getConfigs()
+    );
+});
+
+app.get('/api/selections', (req, res) => {
+    return res.status(200).send(
+        getSelections()
+    );
+});
+
+app.get('/api/user', (req, res) => {
+    return res.status(200).send(
+        state.user
+    );
+});
+
+
+app.post('/api/updateUser', (req, res) => {
+    state.user = req.body; 
+    return res.status(201).send(state.user);
+  });
+
+  app.post('/api/makeSelection', (req, res) => {
+
+      state.selectionsMap[req.body.id].option = req.body.option;
+
+      return res.status(201).send(getSelections());
+    });
+
+const isEnabled = function(config){
+    return config.isEnabledByDefaut || 
+    (config.isEnabledByStreetRacer && state.user.streetRacer)|| 
+    (config.isEnabledByPolitician && state.user.politician);
+}
+
+const getSelections = function(){
+    return state.selections.filter(isEnabled).map(s => {return {id: s.id, option: s.option}}) ;
+}
+
+const getConfigs = function(){
+    return state.configs.filter(isEnabled).map(s => {return {
+        id: s.id, 
+        options: getOptions(s.options),
+        name: s.name    
+    }
+    }) ;
+
+    function getOptions(o) {
+        return state.user.vip ?
+            o.map(_ => { return { ..._, cost: _.cost / 10 } }) :
+            o;
+    } 
+}
+
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
---
+++ state.js
@@ -0,0 +1,45 @@
+
+
+
+const createConfig = (id, name, options, isEnabledByDefaut, isEnabledByStreetRacer, isEnabledByPolitician) => {
+    return {
+        id,
+        name,
+        options,
+        isEnabledByDefaut,
+        isEnabledByStreetRacer,
+        isEnabledByPolitician
+    }
+}
+
+const createOption = (id, name, cost) => { return {id, name, cost};  } 
+
+
+const configs = [
+    createConfig('FARA', 'Фары', [createOption('DEFAULT', '-', 0), createOption('K1', 'Ксенон', 100), createOption('K2', 'Диоды', 200)], true, false, false ),
+    createConfig('SALON', 'Салон', [createOption('DEFAULT', '-', 0),createOption('S1', 'Фанера', 150), createOption('S2', 'Ткань', 200), createOption('S3', 'Кожзам', 300)], true, false, false ),
+    createConfig('MOTOR', 'Мотор', [createOption('DEFAULT', '-', 0),createOption('M1', '100лс', 1500), createOption('M2', '149лс', 2000), createOption('M3', '199лс', 3000)], true, false, false ),
+    createConfig('TUNING', 'Иероглиф на стекло', [createOption('DEFAULT', '-', 0),createOption('T1', 'Банка тушенки', 1500), createOption('T2', 'Свободная касса', 2000), createOption('T3', 'Закрыто на ремонт', 3000)], false, true, false ),
+    createConfig('MIGALKA', 'Мигалка', [createOption('DEFAULT', '-', 0),createOption('MG1', 'Красная', 2000), createOption('MG2', 'Синяя', 3000), createOption('MG3', 'Красная и синяя', 5000)], false, false, true ),
+];
+
+const selections = configs.map(current => {return {
+    id: current.id,
+    option: 'DEFAULT',
+    isEnabledByDefaut: current.isEnabledByDefaut,
+    isEnabledByStreetRacer: current.isEnabledByStreetRacer,
+    isEnabledByPolitician: current.isEnabledByPolitician,
+ }});
+
+const selectionsMap = selections.reduce((acc, current) => {     
+    acc[current.id] = current;    
+    return acc; 
+}, {});
+
+const user = {
+    vip: false,
+    streetRacer: false,
+    politician: false
+}
+
+exports.state = {configs, selections, selectionsMap, user};
